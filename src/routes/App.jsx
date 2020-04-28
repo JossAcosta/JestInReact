082a47b3531d69b83ec372e9ace312a3a8d61971
jossAcosta <josselineacostamtz@gmail.com>
2020-04-28 16:15:01 -0500

    change route

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -9,7 +9,7 @@ const App = () => (
   <BrowserRouter>
     <Layout>
       <Switch>
-        <Route path="/JestInReact" component={Home} />
+        <Route component={Home} />
         <Route exact path="/checkout" component={Checkout} />
         {/* <Route component={NotFound} /> */}
       </Switch>
